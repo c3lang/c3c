56fd22893e449646a264d4757ae0b7ed3af1a3e4
data-man <dataman@tutanota.com>
2022-03-22 03:58:43 +0500

    Update build-with-docker.sh [skip ci]

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -2,12 +2,12 @@
 ## build-with-docker.sh
 ## @author gdm85
 ##
-## Script to build c3c for either Ubuntu 18 or Ubuntu 20.
+## Script to build c3c for either Ubuntu 20, 21 or 22.
 ##
 #
 
 if [ $# -ne 1 -a $# -ne 2 ]; then
-	echo "Usage: build-with-docker.sh (18|20) [Debug|Release]" 1>&2
+	echo "Usage: build-with-docker.sh (20|21|22) [Debug|Release]" 1>&2
 	exit 1
 fi
 
@@ -20,23 +20,27 @@ else
 fi
 
 TAG="$1"
-if [ "$1" = 18 ]; then
-	UBUNTU_VERSION="18.04"
-	DEPS="llvm-10-dev liblld-10-dev libclang-10-dev"
-elif [ "$1" = 20 ]; then
+if [ "$1" = 20 ]; then
 	UBUNTU_VERSION="20.04"
-	DEPS="llvm-11-dev liblld-11-dev clang-11 libllvm11 llvm-11-runtime"
+	LLVM_VERSION="12"
+elif [ "$1" = 21 ]; then
+	UBUNTU_VERSION="21.10"
+	LLVM_VERSION="13"
+elif [ "$1" = 22 ]; then
+	UBUNTU_VERSION="22.04"
+	LLVM_VERSION="14"
 else
-	echo "ERROR: expected 18 or 20 as Ubuntu version argument" 1>&2
+	echo "ERROR: expected 20, 21 or 22 as Ubuntu version argument" 1>&2
 	exit 2
 fi
 
 cd docker && docker build -t c3c-builder:$TAG --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
-	--build-arg DEPS="$DEPS" --build-arg UBUNTU_VERSION="$UBUNTU_VERSION" .
+	--build-arg DEPS="llvm-$LLVM_VERSION-dev liblld-$LLVM_VERSION-dev clang-$LLVM_VERSION libllvm$LLVM_VERSION llvm-$LLVM_VERSION-runtime" \
+	--build-arg UBUNTU_VERSION="$UBUNTU_VERSION" .
 cd ..
 
-rm -rf build bin lib
+rm -rf build bin
 mkdir -p build bin
 
 exec docker run -ti --rm -v "$PWD":/home/c3c/source -w /home/c3c/source c3c-builder:$TAG bash -c \
-	"cd build && cmake -DLLVM_DIR=/usr/lib/llvm-11/cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE .. && cmake --build . && mv c3c ../bin/ && mv lib ../lib"
+	"cd build && cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DC3_LLVM_VERSION=$LLVM_VERSION .. && cmake --build . && mv c3c lib ../bin/"
