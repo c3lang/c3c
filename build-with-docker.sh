1940f6967cd6a3c5b83d5f07ace5a533ef38db4b
gdm85 <gdm85@users.noreply.github.com>
2022-03-23 10:50:52 +0100

    Add support for podman
    Use a tmpfs when running
    Prefix podman images with 'localhost/'

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -13,6 +13,15 @@ fi
 
 set -e
 
+DOCKER=docker
+DOCKER_RUN=""
+IMAGE="c3c-builder"
+if type podman 2>/dev/null >/dev/null; then
+	DOCKER=podman
+	DOCKER_RUN="--userns=keep-id"
+	IMAGE="localhost/$IMAGE"
+fi
+
 if [ -z "$2" ]; then
 	CMAKE_BUILD_TYPE=Debug
 else
@@ -33,8 +42,9 @@ else
 	echo "ERROR: expected 20, 21 or 22 as Ubuntu version argument" 1>&2
 	exit 2
 fi
+IMAGE="$IMAGE:$TAG"
 
-cd docker && docker build -t c3c-builder:$TAG --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
+cd docker && $DOCKER build -t $IMAGE --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
 	--build-arg DEPS="llvm-$LLVM_VERSION-dev liblld-$LLVM_VERSION-dev clang-$LLVM_VERSION libllvm$LLVM_VERSION llvm-$LLVM_VERSION-runtime" \
 	--build-arg UBUNTU_VERSION="$UBUNTU_VERSION" .
 cd ..
@@ -42,5 +52,5 @@ cd ..
 rm -rf build bin
 mkdir -p build bin
 
-exec docker run -ti --rm -v "$PWD":/home/c3c/source -w /home/c3c/source c3c-builder:$TAG bash -c \
+exec $DOCKER run -ti --rm --tmpfs=/tmp $DOCKER_RUN -v "$PWD":/home/c3c/source -w /home/c3c/source $IMAGE bash -c \
 	"cd build && cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DC3_LLVM_VERSION=$LLVM_VERSION .. && cmake --build . && mv c3c lib ../bin/"
