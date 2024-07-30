c0360bf5f063c6348169b4e2d4efa12533b7272f
Chuck Benedict <chuck_benedict@yahoo.com>
2024-07-30 09:03:17 -0700

    Risc-V CI

--- resources/examples/embedded/riscv-qemu/start.s
+++ resources/examples/embedded/riscv-qemu/start.s
@@ -6,11 +6,23 @@
 .section .text._start
 .global _start
 _start:
-    la sp, __stack_top    # Load the stack pointer
-    add s0, sp, zero      # Set the frame pointer
-    jal zero, main        # Run main entry point - no argc
+  la sp, __stack_top      # Load the stack pointer
+  add s0, sp, zero        # Set the frame pointer
+  jal zero, main          # Run main entry point - no argc
 loop:	j loop              # Spin forever in case main returns
-													
+
+# Support semi-hosting calls
+.option norvc
+.text
+.balign 16
+.global sys_semihost
+.type sys_semihost @function
+sys_semihost:             # https://github.com/riscv-non-isa/riscv-semihosting/blob/main/src/binary-interface.adoc
+  slli zero, zero, 0x1f
+  ebreak
+  srai zero, zero, 0x7
+  ret
+
 .section .data
 .space 1024*8             # allocate 8K of memory to serve as initial stack
 .align 16                 # Smallest stack allocation is 16 bytes, so align accordingly
