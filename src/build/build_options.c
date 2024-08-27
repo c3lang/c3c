b884642fa6fc9c75a0e1be1ece5eb812b7d5bbbd
rexim <reximkut@gmail.com>
2024-08-27 07:23:32 +0700

    Print Git Hash on `--version`

--- src/build/build_options.c
+++ src/build/build_options.c
@@ -482,6 +482,9 @@ static void print_version(void)
 	PRINTF("C3 Compiler Version:       %s", COMPILER_VERSION);
 #endif
 	PRINTF("Installed directory:       %s", find_executable_path());
+#ifdef GIT_HASH
+	PRINTF("Git Hash:                  %s", GIT_HASH);
+#endif
 	PRINTF("LLVM version:              %s", llvm_version);
 	PRINTF("LLVM default target:       %s", llvm_target);
 }
