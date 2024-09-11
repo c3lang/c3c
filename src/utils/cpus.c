1b54a99f6a7a248d86aaa7bfe83f95f214f87856
wilsonk <wilsonk@cpsc.ucalgary.ca>
2024-09-11 14:38:53 -0600

    Add initial FreeBSD support (#1430)
    Add initial FreeBSD support

--- src/utils/cpus.c
+++ src/utils/cpus.c
@@ -8,7 +8,7 @@ int cpus(void)
 	GetSystemInfo(&sysinfo);
 	return sysinfo.dwNumberOfProcessors;
 }
-#elif __APPLE__ || defined(__linux__)
+#elif __APPLE__ || defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
 #include <unistd.h>
 int cpus(void)
 {
