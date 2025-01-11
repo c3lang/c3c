e0afc0f9ea5af0dfa61e78209c48a428f130928c
Jefferson Amstutz <jamstutz@nvidia.com>
2025-01-11 10:29:38 -0600

    use WORKING_DIRECTORY to ensure stability of finding the git hash

--- git_hash.cmake
+++ git_hash.cmake
@@ -4,6 +4,7 @@ set(GIT_HASH "unknown")
 
 if(GIT_FOUND AND EXISTS "${CMAKE_CURRENT_LIST_DIR}/.git")
     execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                     OUTPUT_VARIABLE GIT_HASH
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     COMMAND_ERROR_IS_FATAL ANY)
