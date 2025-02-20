9f165342e2f4a8ad3f6040219e2417ccf91a5c26
Christoffer Lerno <christoffer@aegik.com>
2025-02-20 22:41:44 +0100

    Revert disposal

--- src/compiler/llvm_codegen.c
+++ src/compiler/llvm_codegen.c
@@ -82,7 +82,6 @@ static void gencontext_destroy(GenContext *context)
 {
 	ASSERT(llvm_is_global_eval(context));
 	LLVMDisposeBuilder(context->global_builder);
-	LLVMDisposeModule(context->module);
 	if (!context->shared_context) LLVMContextDispose(context->context);
 	LLVMDisposeTargetData(context->target_data);
 	LLVMDisposeTargetMachine(context->machine);
